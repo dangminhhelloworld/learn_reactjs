24171898e78a491e5ae085c0f99ec68a4703699f
Dang Hoang Minh <minhphp2005@gmail.com>
2024-05-11 07:28:53 +0700

    Passing from parent to child

---
+++ src/components/MyComponent.js
@@ -0,0 +1,44 @@
+import React from "react";
+import AddUserInfo from "./AddUserInfo";
+import DisplayInfo from "./DisplayInfo";
+
+class MyComponent extends React.Component {
+ 
+  state = {
+    listUsers: [
+      { id: 1, name: "Dang minh2", age: "16" },
+      { id: 2, name: "Dang minh3", age: "20" },
+      { id: 3, name: "Dang minh3", age: "39" },
+    ],
+  };
+  handleAddNewUser = (userObject) =>{
+    let listUsersClone =[...this.state.listUsers];
+    
+    // this.setState({
+    //   listUsers:[userObject,...this.state.listUsers]
+    // })
+  }
+   //? JSX
+  render() {
+    /*   
+        * chỉ lưu 1 thẻ div
+        * viết js trong dâu //! {} ;
+        
+        
+        */
+    //!DRY: don't repeat yourseft
+    return (
+      <div>
+       <AddUserInfo handleAddNewUser={this.handleAddNewUser}
+        
+       />
+        <br />
+        <DisplayInfo listUsers={this.state.listUsers}
+       
+         />
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
