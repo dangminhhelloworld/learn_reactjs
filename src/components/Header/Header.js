ef7affcaea30b32ceccb4335e35b8fad36eaedef
Dang Hoang Minh <minhphp2005@gmail.com>
2024-05-14 09:01:09 +0700

    Route and link React Hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -11,21 +12,17 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">User</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
-
-
-            
+            <Link to="/"  className="nav-link">Home</Link>
+            <Link to="/users" className="nav-link">Users</Link>
+            <Link to="/admins" className="nav-link">Admins</Link>
           </Nav>
           <Nav>
-              <NavDropdown title="Setting" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Log Out </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-               
-              </NavDropdown>
-            </Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/">Login</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log Out </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Admin from "./components/Admin/Admin";
+import User from "./components/User/User";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
-      <App />
+    <BrowserRouter>
+      <Routes>
+        {" "}
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Routes>
+        {" "}
+        <Route path="/users" element={<User />} />
+      </Routes>
+      <Routes>
+        {" "}
+        <Route path="/admins" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>
 );
